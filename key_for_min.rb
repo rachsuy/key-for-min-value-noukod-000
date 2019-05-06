976474e9c80a9c37e765d5788c3c5c9d6fde899f
Rachele Suy <rachelesuy@gmail.com>
2019-05-06 08:00:46 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,18 +3,14 @@
 
 def key_for_min_value(name_hash)
   
- if name_hash = {}
-    return nil
-  else
-      min_value = name_hash.first[1]
-      min_key = name_hash.first[0]
-      name_hash.each do |key, value|
-        if value < min_value
+     min_value = 0
+      min_key = nil
+      
+      name_hash.collect do |key, value|
+        if value < min_value || min_value == 0
           min_value = value
           min_key = key
         end
       end
-     
-  end 
-  return min_key
+        return  min_key
 end
